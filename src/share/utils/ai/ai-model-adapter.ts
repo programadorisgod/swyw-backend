f27bf44d6c347689ad3309f7b201bc130fe52fde
programadorisgod <catapiascastro@gmail.com>
2025-08-19 16:54:09 -0500

    fix: remove ai-model-adapter.ts

--- src/share/utils/ai/ai-model-adapter.ts
+++
@@ -1,7 +0,0 @@
-import type { AIModel } from './interfaces/ai-model.interface';
-
-export class AIModelAdapter implements AIModel {
-    generate(prompt: string): Promise<string> {
-        throw new Error('Method not implemented.');
-    }
-}
