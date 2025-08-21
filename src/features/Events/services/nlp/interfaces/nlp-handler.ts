a9d5452b2980466c7c557a8e65ec406af356cd6a
programadorisgod <catapiascastro@gmail.com>
2025-08-21 17:36:33 -0500

    chore(events/nlp): remove unused NLP handler classes and interfaces

--- src/features/Events/services/nlp/interfaces/nlp-handler.ts
+++
@@ -1,9 +0,0 @@
-import type { createEventDto, eventDto } from '@src/features/Events/dto/event';
-
-export interface INlpHandler {
-    setNextHandler(handler: INlpHandler): INlpHandler;
-    handle(
-        messageEvent: eventDto,
-        eventData: createEventDto
-    ): Promise<[createEventDto, eventDto]>;
-}
