86979ed67a89b7593d2199062582310015d86603
programadorisgod <catapiascastro@gmail.com>
2025-10-20 18:31:22 -0500

    fix: add 0.0.0.0 ip to listening in k8s

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 import { PORT } from '@src/config/app.config';
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
